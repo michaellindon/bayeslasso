dbecf8659cf64f2bb8d29904e7390824b093958e
Michael Lindon <michael@localhost.localdomain>
2017-04-28 16:47:17 -0400

    initial commit

---
+++ src/bayeslasso/core.clj
@@ -0,0 +1,6 @@
+(ns bayeslasso.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/bayeslasso/core_test.clj
@@ -0,0 +1,7 @@
+(ns bayeslasso.core-test
+  (:require [clojure.test :refer :all]
+            [bayeslasso.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
